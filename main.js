41ff38fbb7289b872d70831a9b04c499d28d5529
Luay Alali <luayalali@gmail.com>
2020-10-10 16:42:44 -0700

    cloned master repo of portfolio

---
+++ main.js
@@ -0,0 +1,97 @@
+
+// jQuery
+
+// navbar
+
+$('.nav-link').on('click',function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  $('.contact-link').on('click',function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  $('.jumbotron').on('click',function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  $('.container-fluid').on('click',function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  $('.bottom-navbar').on('click',function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // cards
+
+//project-1
+  $('.container-fluid').on('click',function() {
+    $('.card-1-collapse').collapse('hide');
+  });
+
+  $('.navbar').on('click',function() {
+    $('.card-1-collapse').collapse('hide');
+  });
+
+
+  $('.bottom-navbar').on('click',function() {
+    $('.card-1-collapse').collapse('hide');
+  });
+
+  //project-2
+  $('.container-fluid').on('click',function() {
+    $('.card-2-collapse').collapse('hide');
+  });
+
+  $('.navbar').on('click',function() {
+    $('.card-2-collapse').collapse('hide');
+  });
+
+
+  $('.bottom-navbar').on('click',function() {
+    $('.card-2-collapse').collapse('hide');
+  });
+
+//project-3
+$('.container-fluid').on('click',function() {
+  $('.card-3-collapse').collapse('hide');
+});
+
+$('.navbar').on('click',function() {
+  $('.card-3-collapse').collapse('hide');
+});
+
+
+$('.bottom-navbar').on('click',function() {
+  $('.card-3-collapse').collapse('hide');
+});
+
+//project-4
+$('.container-fluid').on('click',function() {
+  $('.card-4-collapse').collapse('hide');
+});
+
+$('.navbar').on('click',function() {
+  $('.card-4-collapse').collapse('hide');
+});
+
+
+$('.bottom-navbar').on('click',function() {
+  $('.card-4-collapse').collapse('hide');
+});
+
+//project-5
+$('.container-fluid').on('click',function() {
+  $('.card-5-collapse').collapse('hide');
+});
+
+$('.navbar').on('click',function() {
+  $('.card-5-collapse').collapse('hide');
+});
+
+
+$('.bottom-navbar').on('click',function() {
+  $('.card-5-collapse').collapse('hide');
+});
+
