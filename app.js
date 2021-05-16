f8803da9b37d0dc910420b9de12354ccfa185039
Luay Alali <luayalali@gmail.com>
2021-05-15 21:32:15 -0700

    adjusted padding of navbar and removed left and right margins

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-/* Open when someone clicks on the span element */
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
-  
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+// /* Open when someone clicks on the span element */
+
+// console.log("Hello World");
 
 
-  function toggleIcon() {
-    document.querySelector(".hamburger").style.display = "none";
-  }
+// function openNav() {
+//     document.getElementById("myNav").style.width = "100%";
+//   }
+  
+//   /* Close when someone clicks on the "x" symbol inside the overlay */
+//   function closeNav() {
+//     document.getElementById("myNav").style.width = "0%";
+//   }
 
-  function toggleIconBack() {
-    document.querySelector(".hamburger").style.display = "block";
-  }
+
+//   function toggleIcon() {
+//     document.querySelector(".hamburger").style.display = "none";
+//   }
+
+//   function toggleIconBack() {
+//     document.querySelector(".hamburger").style.display = "block";
+//   }
   
-  var hamburger = document.querySelector(".hamburger");
-  // On click
-  // hamburger.addEventListener("click", function() {
-  //   // Toggle class "is-active"
-  //   hamburger.classList.toggle("is-active");
+//   var hamburger = document.querySelector(".hamburger");
+//   // On click
+//   // hamburger.addEventListener("click", function() {
+//   //   // Toggle class "is-active"
+//   //   hamburger.classList.toggle("is-active");
   
-  // });
+//   // });
+
+console.log("Hello");
+
+
